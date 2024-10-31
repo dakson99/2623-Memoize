76e5daa637b763765783e3e0cc3bf3fd82c7aaea
dakson99 <darko99.mikavica@gmail.com>
2024-10-31 10:11:42 +0100

    Solving the task of an asynchronous function that sleeps in milliseconds

--- Add-Two-Promises.js
+++ Add-Two-Promises.js
@@ -14,7 +14,7 @@ Koristite metod Promise.all() zajedno sa avait da sačekate da se oba obećanja
 
 var addTwoPromises = async function (promise1, promise2) {
 
-    const [value1, value2] = await Promise.all([promise1, promise2]);
+    const [value1, value2] = await Promise.all(promise1, promise2);
 
     return value1 + value2;
 };
---
+++ Sleep.js
@@ -0,0 +1,8 @@
+/*NASLOV ZADATKA:
+Dat je pozitivan ceo broj miliseka, napišite asinhronu funkciju koja spava u milisekundi. Može da reši bilo koju vrednost.
+
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value. */
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
