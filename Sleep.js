ba376d8ee5c56fa55b1844b1f8e4892df4844498
dakson99 <darko99.mikavica@gmail.com>
2024-10-31 10:22:32 +0100

    First solution

--- Sleep.js
+++ Sleep.js
@@ -1,8 +1,16 @@
 /*NASLOV ZADATKA:
 Dat je pozitivan ceo broj miliseka, napišite asinhronu funkciju koja spava u milisekundi. Može da reši bilo koju vrednost.
 
-Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value. */
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+Moramo da napišemo asinhronu funkciju koja spava za dati broj milisekundi, a zatim rešava bilo koju vrednost
+
+*/
 /**
  * @param {number} millis
  * @return {Promise}
  */
+
+async function sleeps(millis) {
+    return new Promise(delayresolve => setTimeout(delayresolve, millis));
+}
