cdbb831b38f4e16266d164c3d156bffa67e7d829
dakson99 <darko99.mikavica@gmail.com>
2024-11-05 09:11:14 +0100

    Solution of the first public method

--- Cache-With-Time-Limit.js
+++ Cache-With-Time-Limit.js
@@ -3,7 +3,7 @@
 Napišite klasu koja omogućava dobijanje i postavljanje parova ključ-vrednost, ali vreme do isteka je povezano sa svakim ključem.
 Klasa ima tri javne metode:
 set(ključ, vrednost, trajanje): prihvata celobrojni ključ, celobrojnu vrednost i trajanje u milisekundama. Kada istekne vreme, ključ bi trebalo da bude nedostupan. Metoda treba da vrati true ako isti ključ koji nije istekao već postoji i false u suprotnom. I vrednost i trajanje bi trebalo da budu zamenjeni ako ključ već postoji.
-get(kei): ako postoji ključ koji nije istekao, trebalo bi da vrati pridruženu vrednost. U suprotnom bi trebalo da vrati -1.
+get(key): ako postoji ključ koji nije istekao, trebalo bi da vrati pridruženu vrednost. U suprotnom bi trebalo da vrati -1.
 count(): vraća broj ključeva koji nisu istekli.
 
 Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
@@ -12,3 +12,26 @@ set(key, value, duration): accepts an integer key, an integer value, and a durat
 get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
 count(): returns the count of un-expired keys.
 */
+
+var TimeLimitedCache = function () {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    let found = this.cache.has(key);
+    if (found)
+        clearTimeout(this.cache.get(key).ref);
+    this.cache.set(key, {
+        value,
+        ref: setTimeout(() => this.cache.delete(key), duration)
+    });
+    return found;
+};
+
