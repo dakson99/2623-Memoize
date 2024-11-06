0c7c428612b5375fce7173a43f96ab6ac3221b26
dakson99 <darko99.mikavica@gmail.com>
2024-11-06 09:55:06 +0100

    The title of the task: Chunk Array

---
+++ Chunk-Array.js
@@ -0,0 +1,18 @@
+/* NASLOV ZADATKA
+
+Dati niz arr i veličinu komada, vratite niz podeljen na komade.
+
+Niz u komadima sadrži originalne elemente u arr, ali se sastoji od podnizova veličine dužine. Dužina poslednjeg podniza može biti manja od veličine ako arr.length nije ravnomerno deljiva veličinom.
+
+Možete pretpostaviti da je niz izlaz JSON.parse. Drugim rečima, to je važeći JSON.
+
+Rešite ga bez upotrebe lodash-ove _.chunk funkcije.
+
+Given an array arr and a chunk size size, return a chunked array.
+
+A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+
+You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+
+Please solve it without using lodash's _.chunk function.
+*/
