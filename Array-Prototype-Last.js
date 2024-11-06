0f44ddc822f39e0c33906cd92567e8807239483a
dakson99 <darko99.mikavica@gmail.com>
2024-11-06 10:30:16 +0100

    The title of the task: Array Prototype Last

---
+++ Array-Prototype-Last.js
@@ -0,0 +1,10 @@
+/* NASLOV ZADATKA
+
+Napišite kod koji poboljšava sve nizove tako da možete pozvati metod arrai.last() na bilo kom nizu i on će vratiti poslednji element. Ako u nizu nema elemenata, trebalo bi da vrati -1.
+
+Možete pretpostaviti da je niz izlaz JSON.parse
+
+Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+You may assume the array is the output of JSON.parse.
+*/
