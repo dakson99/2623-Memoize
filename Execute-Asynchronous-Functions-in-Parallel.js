ea0204f528b6859eec9f23aa0171e8cef6363c2f
dakson99 <darko99.mikavica@gmail.com>
2024-11-06 08:28:25 +0100

    Solution the task

--- Execute-Asynchronous-Functions-in-Parallel.js
+++ Execute-Asynchronous-Functions-in-Parallel.js
@@ -20,3 +20,28 @@ promise rejects:
 When any of the promises returned from functions were rejected. promise should also reject with the reason of the first rejection.
 Please solve it without using the built-in Promise.all function.
 */
+
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+
+var promiseAll = async function (functions) {
+    return new Promise((resolve, reject) => {
+        const results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i) => {
+            fn()
+                .then(val => {
+                    results[i] = val;
+                    count++;
+                    if (count === functions.length)
+                        resolve(results);
+                })
+                .cache(reason => reject(reason));
+        });
+    });
+};
+
+const promise = promiseAll([() => new Promise(res => res(42))])
+promise.then(console.log);
