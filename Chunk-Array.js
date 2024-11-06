935273a65e452329928df7812d9c5a24a6010d91
dakson99 <darko99.mikavica@gmail.com>
2024-11-06 10:02:47 +0100

    Task solution: Chunk Array

--- Chunk-Array.js
+++ Chunk-Array.js
@@ -1,7 +1,7 @@
 /* NASLOV ZADATKA
 
 Dati niz arr i veličinu komada, vratite niz podeljen na komade.
-
+umesto [1,2,3,4,5] ide [1,2], [3,4], [5,6]
 Niz u komadima sadrži originalne elemente u arr, ali se sastoji od podnizova veličine dužine. Dužina poslednjeg podniza može biti manja od veličine ako arr.length nije ravnomerno deljiva veličinom.
 
 Možete pretpostaviti da je niz izlaz JSON.parse. Drugim rečima, to je važeći JSON.
@@ -16,3 +16,22 @@ You may assume the array is the output of JSON.parse. In other words, it is vali
 
 Please solve it without using lodash's _.chunk function.
 */
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+
+var chunk = function (arr, size) {
+    const chunkedArray = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArray.push(arr.slice(i, i + size));
+    }
+
+    return chunkedArray;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6], 2)); // [[1, 2], [3, 4], [5, 6]]
+console.log(chunk([1, 2, 3, 4, 5], 3)); // [[1, 2, 3], [4, 5]]
