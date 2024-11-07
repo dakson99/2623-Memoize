89b36df9d2ef9a689c140e634938dfe57d65190e
dakson99 <darko99.mikavica@gmail.com>
2024-11-07 08:27:16 +0100

    The name: Group-By

---
+++ Group-By.js
@@ -0,0 +1,22 @@
+/* NASLOV ZADATKA
+
+Napišite kod koji poboljšava sve nizove tako da možete pozvati metod arrai.groupBi(fn) na bilo kom nizu i on će vratiti grupisanu verziju niza.
+
+Grupisani niz je objekat gde je svaki ključ izlaz fn(arr[i]), a svaka vrednost je niz koji sadrži sve stavke u originalnom nizu koje generišu taj ključ.
+
+Obezbeđeni povratni poziv fn će prihvatiti stavku u nizu i vratiti ključ niza.
+
+Redosled svake liste vrednosti treba da bude redosled kojim se stavke pojavljuju u nizu. Svaki redosled ključeva je prihvatljiv.
+
+Rešite ga bez lodashove _.groupBi funkcije.
+
+Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+
+A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array which generate that key.
+
+The provided callback fn will accept an item in the array and return a string key.
+
+The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
+
+Please solve it without lodash's _.groupBy function.
+*/ 
