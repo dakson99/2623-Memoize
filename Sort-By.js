0f710c906eafe2effba5f147a322589df11203a0
dakson99 <darko99.mikavica@gmail.com>
2024-11-07 08:56:31 +0100

    The title of the task: Sort-by

---
+++ Sort-By.js
@@ -0,0 +1,10 @@
+/* NASLOV ZADATKA
+
+Dati niz arr i funkciju fn, vratite sortirani niz sortedArr. Možete pretpostaviti da fn vraća samo brojeve i da ti brojevi određuju redosled sortiranja sortedArr-a. sortedArr mora biti sortiran u rastućem redosledu prema fn izlazu.
+
+Možete pretpostaviti da fn nikada neće duplirati brojeve za dati niz.
+
+Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. sortedArr must be sorted in ascending order by fn output.
+
+You may assume that fn will never duplicate numbers for a given array.
+*/ 
