7c548e6fb384ef85dd4b244559b53f5f116df754
dakson99 <darko99.mikavica@gmail.com>
2024-11-11 07:55:16 +0100

    Task title: Flatten Deeply Nested Array

---
+++ Flatten-Deeply-Nested-Array.js
@@ -0,0 +1,18 @@
+/*
+
+Dati višedimenzionalni niz arr i dubinu n, vratite spljoštenu verziju tog niza.
+
+Višedimenzionalni niz je rekurzivna struktura podataka koja sadrži cele brojeve ili druge višedimenzionalne nizove.
+
+Spljošteni niz je verzija tog niza sa nekim ili svim podnizovima uklonjenim i zamenjenim stvarnim elementima u tom podnizu. Ovu operaciju izravnavanja treba uraditi samo ako je trenutna dubina gnežđenja manja od n. Dubina elemenata u prvom nizu se smatra 0.
+
+Rešite ga bez ugrađene metode Arrai.flat
+
+Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
+
+A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
+
+A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array. This flattening operation should only be done if the current depth of nesting is less than n. The depth of the elements in the first array are considered to be 0.
+
+Please solve it without the built-in Array.flat method
+*/
