ead75fcccb1d45b1c0bbb2bc679c1612555a2237
dakson99 <darko99.mikavica@gmail.com>
2024-11-11 08:26:47 +0100

    Solving the task: Flatten Deeply Nested Array

--- Flatten-Deeply-Nested-Array.js
+++ Flatten-Deeply-Nested-Array.js
@@ -16,3 +16,31 @@ A flattened array is a version of that array with some or all of the sub-arrays 
 
 Please solve it without the built-in Array.flat method
 */
+
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+
+var flat = function (arr, depth) {
+    const stack = [...arr.map(item => [item, depth])];
+    const result = [];
+
+    while (stack.length > 0) {
+        const [item, depth] = stack.pop();
+
+        if (Array.isArray(item) && depth > 0) {
+            stack.push(...item.map(subItem => [subItem, depth - 1]));
+        } else {
+            result.push(item);
+        }
+    }
+
+    return result.reverse();
+};
+
+let arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
+let depth = 2;
+
+console.log(flat(arr, depth));
