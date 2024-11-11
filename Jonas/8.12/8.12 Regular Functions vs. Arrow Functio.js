f40a59fc297a3aa02259018672eff2d38d7a24be
dakson99 <darko99.mikavica@gmail.com>
2024-11-11 10:33:58 +0100

    Commit 3

--- Jonas/8.12/8.12 Regular Functions vs. Arrow Functio.js
+++ Jonas/8.12/8.12 Regular Functions vs. Arrow Functio.js
@@ -4,23 +4,34 @@ U ovom slucaju smo stavili var firstname Matilda i radilo je ali to ne treba kor
 U praksi nikada ne bi trebalo da koristmo arrow funkciju kao metod.
 Kada bi koristili regularnu funkciju umesto arrow i sada metodaa greet
 dobija svoju this keyword
+Pravimo funkciju koja ce u osnovi vratiti ako je osoba milenium
+Posotje dva resenja za ovaj problem:
+Prva solucija jeste koriscenjem dodatne promenljive koju nazivamo self koja ide van funkcije, jer ovako smo van isMillenial funckije. Ovo je bilo resenje pre ES6 
 
 */
-var firstName = 'Matilda';
+// var firstName = 'Matilda';
 const Darko = {
     firstName: 'Darko',
     year: 1999,
     calcAge: function () {
-        console.log(this);
+        // console.log(this);
         console.log(2037 - this.year);
+
+        const self = this;
+        const isMillenial = function () {
+            console.log(self);
+            console.log(self.year >= 1992 && self.year <= 2007);
+        };
+        //console.log(this.year >= 1992 && this.year <= 2007); };
+        isMillenial();
     },
 
-    greet: function () {
+    greet: () => {
         console.log(this);
         console.log(`Hey ${this.firstName}`);
     },
 };
 
 Darko.greet();
-console.log(this.firstName);
+Darko.calcAge();
 
