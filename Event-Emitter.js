e1752f08a2910938ebf7e02e8eaa85e7620289a5
dakson99 <darko99.mikavica@gmail.com>
2024-11-12 08:40:09 +0100

    Solving the task Event Emitter

--- Event-Emitter.js
+++ Event-Emitter.js
@@ -17,4 +17,44 @@ subscribe - This method takes in two arguments: the name of an event as a string
 An event should be able to have multiple listeners for the same event. When emitting an event with multiple callbacks, each should be called in the order in which they were subscribed. An array of results should be returned. You can assume no callbacks passed to subscribe are referentially identical.
 The subscribe method should also return an object with an unsubscribe method that enables the user to unsubscribe. When it is called, the callback should be removed from the list of subscriptions and undefined should be returned.
 emit - This method takes in two arguments: the name of an event as a string and an optional array of arguments that will be passed to the callback(s). If there are no callbacks subscribed to the given event, return an empty array. Otherwise, return an array of the results of all callback calls in the order they were subscribed.
-*/ 
+*/
+
+
+class EventEmitter {
+    constructor() {
+        this.events = new Map();
+    }
+
+    subscribe(event, cb) {
+        if (!this.events.has(event)) {
+            this.events.set(event, []);
+        }
+
+        const listeners = this.events.get(event);
+        listeners.push(cb);
+
+        return {
+            unsubscribe: () => {
+                const index = listeners.indexOf(cb);
+                if (index !== -1) {
+                    listeners.splice(index, 1);
+                }
+            }
+        };
+    }
+
+    emit(event, args = []) {
+        if (!this.events.has(event)) {
+            return [];
+        }
+
+        const listeners = this.events.get(event);
+        const results = [];
+
+        for (const listener of listeners) {
+            results.push(listener(...args));
+        }
+
+        return results;
+    }
+}
