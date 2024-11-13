e31619e258f8474c45f069b8e386fd765a94320a
dakson99 <darko99.mikavica@gmail.com>
2024-11-13 20:12:34 +0100

    New task; Array Wrapper

---
+++ Array-Wrapper.js
@@ -0,0 +1,12 @@
+/* NASLOV ZADATKA
+
+Kreirajte klasu ArraiVrapper koja prihvata niz celih brojeva u svom konstruktoru. Ova klasa treba da ima dve karakteristike:
+
+Kada se dve instance ove klase saberu zajedno sa + operatorom, rezultujuća vrednost je zbir svih elemenata u oba niza.
+Kada se funkcija String() pozove na instanci, ona će vratiti string odvojen zarezima okružen zagradama. Na primer, [1,2,3]
+
+Create a class ArrayWrapper that accepts an array of integers in its constructor. This class should have two features:
+
+When two instances of this class are added together with the + operator, the resulting value is the sum of all the elements in both arrays.
+When the String() function is called on the instance, it will return a comma separated string surrounded by brackets. For example, [1,2,3]
+*/
