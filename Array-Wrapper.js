d846c0a198478860d41720e692f47098e9a7aaea
dakson99 <darko99.mikavica@gmail.com>
2024-11-14 08:13:06 +0100

    The valueOf method calculate the sum of element in nums

--- Array-Wrapper.js
+++ Array-Wrapper.js
@@ -19,3 +19,14 @@ var ArrayWrapper = function (nums) {
     this.nums = nums;
 }
 
+/**
+ * @return {number}
+ */
+
+ArrayWrapper.prototype.valueOf = function () {
+    let sum = 0;
+    for (let i = 0; i < this.nums.length; i++) {
+        sum += this.nums[i];
+    }
+    return sum;
+}
