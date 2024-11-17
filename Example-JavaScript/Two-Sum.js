1479455eb96b545d10305eaa73f22098c45a8794
dakson99 <darko99.mikavica@gmail.com>
2024-11-17 21:17:46 +0100

    Name of the task: Two Sum, Serbian and English

---
+++ Example-JavaScript/Two-Sum.js
@@ -0,0 +1,14 @@
+/*NASLOV ZADATKA
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ Dati niz celih brojeva nums i celobrojni cilj, vratite indekse dva broja tako da se sabiraju u cilj.
+
+Možete pretpostaviti da bi svaki ulaz imao tačno jedno rešenje i da ne možete koristiti isti element dvaput.
+
+Odgovor možete vratiti bilo kojim redosledom.
+*/ 
