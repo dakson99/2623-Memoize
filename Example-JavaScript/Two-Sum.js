9238563fb82b407b1a329345c405195641fe602d
dakson99 <darko99.mikavica@gmail.com>
2024-11-18 08:25:54 +0100

    Solving the task: two-sum

--- Example-JavaScript/Two-Sum.js
+++ Example-JavaScript/Two-Sum.js
@@ -11,4 +11,36 @@ You can return the answer in any order.
 Možete pretpostaviti da bi svaki ulaz imao tačno jedno rešenje i da ne možete koristiti isti element dvaput.
 
 Odgovor možete vratiti bilo kojim redosledom.
-*/ 
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+    let numToIndexMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+
+        let diff = target - nums[i];
+
+
+        if (numToIndexMap.hasOwnProperty(diff)) {
+
+            return [i, numToIndexMap[diff]];
+        }
+
+
+        numToIndexMap[nums[i]] = i;
+    }
+
+
+    return null;
+};
+
+let nums = [1, 3, 4, 8];
+let target = 7;
+
+console.log(twoSum(nums, target));
