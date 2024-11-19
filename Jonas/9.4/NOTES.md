415a95eaa7a94cbd044d423ebeeac83f382a40e5
dakson99 <darko99.mikavica@gmail.com>
2024-11-19 09:06:37 +0100

    I added an openingHourse object the restaurant and added a basic destructuring example

---
+++ Jonas/9.4/NOTES.md
@@ -0,0 +1,4 @@
+Destrukturiranje objekata
+
+Koristimo viticanste zagrade za destrukturiranje, jer na ovaj nacin stvaramo i objekat.
+U objektima redosled elemenata nije bitan.
---
+++ Jonas/9.4/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavento 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vagetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHourse: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+};
+
+const { name, openingHourse, categories } = restaurant;
+console.log(name, openingHourse, categories);
+
+const { name: restaurantName, openingHourse: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
