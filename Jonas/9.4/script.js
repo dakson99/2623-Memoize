e7155fd48a28b9f3de345885ab5089927dd33d5f
dakson99 <darko99.mikavica@gmail.com>
2024-11-19 09:20:08 +0100

    If we want to set a propery to be empty, we add visible brackets.

--- Jonas/9.4/script.js
+++ Jonas/9.4/script.js
@@ -26,8 +26,14 @@ const restaurant = {
     },
 };
 
+// Klasican primer destrukturiranja
 const { name, openingHourse, categories } = restaurant;
 console.log(name, openingHourse, categories);
 
 const { name: restaurantName, openingHourse: hours, categories: tags } = restaurant;
 console.log(restaurantName, hours, tags);
+
+// Ovde je pokazano destrukturiranje ali je dodata podrazumevana vrednost
+// viticaste zagrade da se postavi prazna vrednost, jer u ovom primeru nemamo nista pod name svojstvom, kad bi u name izbrisali [] dobili bi undefine
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
