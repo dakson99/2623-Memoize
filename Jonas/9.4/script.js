bebd820957bdf25f5136ab3cddbafbbb5f1c0724
dakson99 <darko99.mikavica@gmail.com>
2024-11-19 10:06:32 +0100

    Passing an object a function as an argument

--- Jonas/9.4/script.js
+++ Jonas/9.4/script.js
@@ -24,7 +24,24 @@ const restaurant = {
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+        );
+    }
 };
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Solde, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+    address: 'Via del Sole, 21',
+    starterIndex: 1,
+});
 
 // Klasican primer destrukturiranja
 const { name, openingHourse, categories } = restaurant;
