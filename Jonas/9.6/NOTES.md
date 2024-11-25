a197953ceda5b4befd86fe47bede26d731f7c903
dakson99 <darko99.mikavica@gmail.com>
2024-11-25 14:35:01 +0100

    Using the rest pattern with an array, object and function

--- Jonas/9.6/NOTES.md
+++ Jonas/9.6/NOTES.md
@@ -1,4 +1,4 @@
-Resta patern i rest parametar
+Resta patern i spread operator
 Rest patter izgleda isto kao spread operator i radi suprotno u odnosu na spread operator. 
 Zamislimo da smo spread operator koristili za izradu novog niza ili da prenesmo vise vrednosti u funkciju i to su dva slucaja gde koristimo spreda operato, i u oba slucaja koristimo spread operator da prosirimo niz na pojedinacne elemente.
 Rest pattern korisimo istu sintaksu(...) pristupamo vise elemenata i kondenzujemo ga u niz.
--- Jonas/9.6/script.js
+++ Jonas/9.6/script.js
@@ -1,7 +1,50 @@
 
+// 1) destrukturiranje
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 
 // REST, because on LEFT side of =
 const [a, b, ...others] = [1, 2, 3, 4, 5];
 console.log(a, b, others);
+
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavento 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vagetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHourse: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+}
+
+// Ispisali smo prvi i treci element u nizu i na kraju obavezno ide rest patter poslje njega ne mozemo nista da dodajemo.
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(pizza, risotto, otherFood);
+
+// Sad sa objektina
+const { sat, ...wekekdays } = restaurant.openingHourse
+console.log(wekekdays);
+
+// 2) funcije
+const add = function (...numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++)
+        sum += numbers[i];
+    console.log(sum);
+}
+add(2, 3);
+add(5, 3, 7, 8);
+add(8, 2, 5, 3, 1, 6, 8);
