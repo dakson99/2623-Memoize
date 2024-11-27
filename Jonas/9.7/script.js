9dd22a879662fef0865f98a84c3609543e8258a1
dakson99 <darko99.mikavica@gmail.com>
2024-11-27 14:34:58 +0100

    Short Circuiting && and

---
+++ Jonas/9.7/script.js
@@ -0,0 +1,7 @@
+// Use ANY data type, return ANY data type,short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Helo' || 23 || null);
