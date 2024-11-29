be6b769d35d493961e2b874a78f2b804523acafd
dakson99 <darko99.mikavica@gmail.com>
2024-11-29 14:31:59 +0100

    Logical assigment operators. First and operator

---
+++ Jonas/9.9/script.js
@@ -0,0 +1,16 @@
+const rest1 = {
+    name: 'Capri',
+    numGuests: 20,
+};
+
+const rest2 = {
+    name: 'La Piazza',
+    owner: 'Giovanni Rossi',
+};
+
+// Ovaj primer radi tako sto rest 1 da li je jedank sa rest 1 jeste i u prvom slucaju vraca 20 posto je numGuests 20, a u drugomj liniji koda numGuests nije = sa rest2numGuests i preskace to i ide na sledece to je false vrenost 10.
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+console.log(rest1);
+console.log(rest2);
