8835ce727786e0cbe9dd55b9f1614953fad0f7c2
dakson99 <darko99.mikavica@gmail.com>
2024-12-20 08:52:26 +0100

    Bind methode with partial application

--- Jonas/10.9/script.js
+++ Jonas/10.9/script.js
@@ -48,6 +48,24 @@ lufthausa.buyPlane = function () {
 }
 //lufthausa.buyPlane();
 
-document
-    .querySelector('.buy')
-    .addEventListener('click', lufthausa.buyPlane.bind(lufthausa));
+//document.querySelector('.buy').addEventListener('click', lufthausa.buyPlane.bind(lufthausa));
+
+// Partial application, za ovo unapred mozemo postaviti parametre
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = values => values + values * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+    return function (value) {
+        return value + value * rate;
+    };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
