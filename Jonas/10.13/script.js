8c8c5311ecfa50f2555e798bb48306ce24f115e7
dakson99 <darko99.mikavica@gmail.com>
2024-12-25 09:11:14 +0100

    More Closure Example 1

---
+++ Jonas/10.13/script.js
@@ -0,0 +1,26 @@
+// Example 1
+
+let f;
+
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    };
+};
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
