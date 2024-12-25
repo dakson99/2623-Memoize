f3f5cf894d3b2babeab3ad27cf45f53ff93f4cc7
dakson99 <darko99.mikavica@gmail.com>
2024-12-25 10:02:04 +0100

    More Closure Examples 2

--- Jonas/10.13/script.js
+++ Jonas/10.13/script.js
@@ -24,3 +24,19 @@ console.dir(f);
 h();
 f();
 console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now borading all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
