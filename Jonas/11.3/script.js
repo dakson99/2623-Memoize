d004eeda7b8f1f1f1c6afb890d6f8572d601e70d
dakson99 <darko99.mikavica@gmail.com>
2024-12-30 09:14:52 +0100

    Reverse method

--- Jonas/11.3/script.js
+++ Jonas/11.3/script.js
@@ -94,3 +94,11 @@ console.log([...arr]);
 //console.log(arr.splice(2));
 arr.splice(-1);
 console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
