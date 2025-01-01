59a8f5128584a5018542eb54b6200cf9125093b8
dakson99 <darko99.mikavica@gmail.com>
2025-01-01 19:49:55 +0100

    Map method

---
+++ Jonas/11.11/NOTES.md
@@ -0,0 +1,2 @@
+MAPa je jos jedan nacin da predjemo preko niza. Metoda mape nam daje potpuno novi niz.
+Metoda MAP ne mutira niz, nego vraca novi niz sa novim elementima.
---
+++ Jonas/11.11/script.js
@@ -0,0 +1,9 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1
+const movementsUSD = movements.map(function (mov) {
+    return mov * eurToUsd;
+    //return 23; u svakoji iteraciji originalnog niza jednostavno stavi 23 u svaki niz na istoj poziciji. 
+});
+console.log(movements);
+console.log(movementsUSD);
