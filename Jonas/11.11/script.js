1222bd26f988adf1cc8db2c16bc140115589a5f8
dakson99 <darko99.mikavica@gmail.com>
2025-01-01 19:57:31 +0100

    The first example is solved using the arrow function instead of the function

--- Jonas/11.11/script.js
+++ Jonas/11.11/script.js
@@ -1,13 +1,18 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1
-const movementsUSD = movements.map(function (mov) {
-    return mov * eurToUsd;
-    //return 23; u svakoji iteraciji originalnog niza jednostavno stavi 23 u svaki niz na istoj poziciji. 
-});
+//const movementsUSD = movements.map(function (mov) {
+//return mov * eurToUsd;
+//return 23; u svakoji iteraciji originalnog niza jednostavno stavi 23 u svaki niz na istoj poziciji. 
+//});
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+
 console.log(movements);
 console.log(movementsUSD);
 
 const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
 console.log(movementsUSDfor);
+
