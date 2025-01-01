aff8d0d6ac0de4b551163dfef49a929f07cca043
dakson99 <darko99.mikavica@gmail.com>
2025-01-01 20:14:28 +0100

    MovementDescirptions

--- Jonas/11.11/script.js
+++ Jonas/11.11/script.js
@@ -16,3 +16,11 @@ const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
 console.log(movementsUSDfor);
 
+const movementsDescriptions = movements.map((mov, i, arr) => {
+    if (mov > 0) {
+        return `Movement ${i + 1}: You deposited ${mov}`;
+    } else {
+        return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+    }
+});
+console.log(movementsDescriptions);
