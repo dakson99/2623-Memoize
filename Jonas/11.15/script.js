d1f0975fcaf0e17ce3d52ac314d294d6ae26b911
dakson99 <darko99.mikavica@gmail.com>
2025-01-13 11:48:17 +0100

    3. Calculate the average human age of all adult dogs

--- Jonas/11.15/script.js
+++ Jonas/11.15/script.js
@@ -21,6 +21,11 @@ const calcAverageHumanAge = function (ages) {
     const adults = humanAge.filter(age => age >= 18);
     console.log(humanAge);
     console.log(adults);
-}
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
+    const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+    return average;
+}
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
