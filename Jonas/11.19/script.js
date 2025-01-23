8c319b3a4e82c09038edd83afcbb185782475c61
dakson99 <darko99.mikavica@gmail.com>
2025-01-23 08:54:42 +0100

    Implementing Transfers if logic

--- Jonas/11.19/script.js
+++ Jonas/11.19/script.js
@@ -95,9 +95,9 @@ const displayMovements = function (movements) {
     });
 }
 
-const calcDisplayBalance = function (movements) {
-    const balance = movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${balance}€`;
+const calcDisplayBalance = function (acc) {
+    acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${acc.balance}€`;
 }
 
 const calcDisplaySummary = function (acc) {
@@ -133,6 +133,15 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
+const updateUI = function (acc) {
+    // Display movements
+    displayMovements(acc.movements);
+    //Display balance
+    calcDisplayBalance(acc);
+    //Display summary
+    calcDisplaySummary(acc);
+}
+
 //Event handler
 let currentAccount;
 
@@ -154,13 +163,8 @@ btnLogin.addEventListener('click', function (e) {
         inputLoginUsername.value = inputLoginPin.value = '';
         inputLoginPin.blur();
 
-        // Display movements
-        displayMovements(currentAccount.movements);
-        //Display balance
-        calcDisplayBalance(currentAccount.movements);
-        //Display summary
-        calcDisplaySummary(currentAccount);
-
+        // Update UI
+        updateUI(currentAccount);
     }
 });
 
@@ -170,7 +174,19 @@ btnTransfer.addEventListener('click', function (e) {
     const receiveAcc = accounts.find(
         acc => acc.username === inputTransferTo.value
     );
-    console.log(amount, receiveAcc);
+    inputTransferAmount.value = inputTransferTo.value = '';
+
+    if (amount > 0 &&
+        receiveAcc &&
+        currentAccount.balance >= amount && receiveAcc?.username !== currentAccount.username
+    ) {
+        // Doint the transfer
+        currentAccount.movements.push(-amount);
+        receiveAcc.movements.push(amount);
+
+        //Update UI
+        updateUI(currentAccount);
+    }
 });
 
 /////////////////////////////////////////////////
