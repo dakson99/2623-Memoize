610358b2d952682392d626e928d81b0b3347000d
dakson99 <darko99.mikavica@gmail.com>
2025-02-06 11:46:41 +0100

    Sarah cares whether she eats too much or too little.

--- Jonas/11.19/script.js
+++ Jonas/11.19/script.js
@@ -395,6 +395,23 @@ console.log(convertTitleCase('this is a nice title'));
 console.log(convertTitleCase('title is a LONG title but not too long'));
 console.log(convertTitleCase('and here is anothe title with an EXAMPLE'));
 
+/*
+OUR TASKS:
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, too little or the exact amount of food, based on the recommended food portion.
+9. Group the dogs by the number of owners they have
+10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+*/
 // Coding challange
 
 const dogs = [
@@ -409,3 +426,9 @@ const dogs = [
 dogs.forEach(dog => (dog.recFood = Math.floor(dog.weight ** 0.75 * 28)));
 console.log(dogs);
 
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+    `Sarah's dog eats too ${dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+    }`
+);
