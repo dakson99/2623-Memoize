5673a50d5ffe2389dd0816b2a29510803be4aaf5
dakson99 <darko99.mikavica@gmail.com>
2025-02-12 10:49:29 +0100

    Remainder Operator

--- Jonas/12.3/script.js
+++ Jonas/12.3/script.js
@@ -319,3 +319,10 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
